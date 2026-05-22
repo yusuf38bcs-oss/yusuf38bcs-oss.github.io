0b1057bc8ecf6fa070a0c7dbbd497dfdfbb52da7
Md. Yusuf <yusuf.38bcs@gmail.com>
2026-05-22 08:30:58 +0600

    Finalize menu fixes and link custom assets

---
+++ assets/js/menu-scroll.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener('scroll', () => {
+    const header = document.querySelector('.site-header');
+    if (header) {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    }
+});
